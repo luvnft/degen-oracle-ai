0af2252844484cf4b869f868a938e341d3a59410
Twutc C <twutcc@Twutcs-Mac-mini.local>
2024-12-19 03:25:46 +0700

    Update build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ชื่อ-repository/',
   build: {
     outDir: 'dist',
     sourcemap: true
